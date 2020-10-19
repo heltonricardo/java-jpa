ad21592ef4e5ce7c1ecc074a5b78d2fedd0e9fe8
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-10-19 12:31:30 -0300

    Create NovoUsuario.java

---
+++ teste/basico/NovoUsuario.java
@@ -0,0 +1,26 @@
+package teste.basico;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import modelo.basico.Usuario;
+
+public class NovoUsuario {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("exercicios-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		Usuario novoUsuario = new Usuario("Ana", "ana@email.com");
+		
+		em.getTransaction().begin();
+		em.persist(novoUsuario);
+		em.getTransaction().commit();
+		
+		em.close();
+		emf.close();
+	}
+}
