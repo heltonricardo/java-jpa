a334752a517cdfa2c5d94a563a5c695f08183bc1
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-10-20 11:46:36 -0300

    Create ObterUsuarios.java

---
+++ teste/basico/ObterUsuarios.java
@@ -0,0 +1,36 @@
+package teste.basico;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import modelo.basico.Usuario;
+
+public class ObterUsuarios {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("exercicios-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		String jpql = "SELECT u FROM Usuario u";
+		TypedQuery<Usuario> query = em.createQuery(jpql, Usuario.class);
+		query.setMaxResults(5);
+		
+		List<Usuario> usuarios = query.getResultList();
+		
+		for (Usuario usuario : usuarios) {
+			System.out.println(usuario.getId());
+			System.out.println(usuario.getNome());
+			System.out.println(usuario.getEmail());
+			System.out.println();
+		}
+				
+		emf.close();
+		em.close();
+	}
+}
