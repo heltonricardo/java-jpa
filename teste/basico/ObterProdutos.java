7d2b450e4d6df663ec92721d43892a151d8f6ed4
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-10-29 14:01:57 -0300

    Create ObterProdutos.java

---
+++ teste/basico/ObterProdutos.java
@@ -0,0 +1,29 @@
+package teste.basico;
+
+import java.util.List;
+
+import infra.ProdutoDAO;
+import modelo.basico.Produto;
+
+public class ObterProdutos {
+
+	public static void main(String[] args) {
+		
+		ProdutoDAO dao = new ProdutoDAO();
+		
+		List<Produto> produtos = dao.obterTodos();
+		
+		for (Produto produto : produtos)
+			System.out.println(produto.getId() + ": " +
+					produto.getNome() + " (R$ " +
+					produto.getPreco() + ")");
+		
+		double pretoTotal = produtos
+				.stream()
+				.map(p -> p.getPreco())
+				.reduce(0.0, (t, p) -> t + p)
+				.doubleValue();
+		
+		System.out.printf("Preço total: R$ %.2f", pretoTotal);
+	}
+}
