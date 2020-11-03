610f205d3d5a44ef7eca89ebff09289a129243db
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-11-03 17:43:09 -0300

    Create ItemPedido.java

---
+++ modelo/umpramuitos/ItemPedido.java
@@ -0,0 +1,82 @@
+package modelo.umpramuitos;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import modelo.basico.Produto;
+
+@Entity
+@Table(name = "ITENS_PEDIDO")
+public class ItemPedido {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@ManyToOne
+	private Pedido pedido;
+	
+	@ManyToOne
+	private Produto produto;
+	
+	@Column(nullable = false)
+	private int quantidade;
+	
+	@Column(nullable = false)
+	private Double preco;
+	
+	public ItemPedido() {}
+
+	public ItemPedido(Pedido pedido, Produto produto, int quantidade) {
+		this.setPedido(pedido);
+		this.setProduto(produto);
+		this.setQuantidade(quantidade);
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+		
+		if (produto != null && preco == null)
+			setPreco(produto.getPreco());
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+}
