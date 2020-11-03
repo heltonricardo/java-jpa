5869b5daa06f3b03f02d7eceba4b810aec81b4b6
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-11-03 13:37:04 -0300

    Update Assento.java

--- modelo/umpraum/Assento.java
+++ modelo/umpraum/Assento.java
@@ -4,7 +4,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+/*
+ * Para criar a relação bidirecional usamos a notação @OneToOne em um novo
+ * atributo na entidade Assento. A anotação simples fará com que sejam criados
+ * novos atributos/chaves estrangeiras em ambas as entidades (o que
+ * proporciona grande possibilidade de inconsistência de dados), por isso
+ * usamos a propriedade mappedBy. Nela é informado o atributo que conterá a
+ * relação um pra um na outra tabela, permitindo o uso da bidirecionalidade com
+ * a criação de chave chave estrangeira somente em uma das tabelas (na que não
+ * contém o mappedBy).
+ */
 
 @Entity @Table(name = "ASSENTOS")
 public class Assento {
@@ -13,6 +25,9 @@ public class Assento {
 	private Long id;
 	
 	private String nome;
+	
+	@OneToOne(mappedBy = "assento")
+	private Cliente cliente;
 
 	public Assento() {}
 	
@@ -34,5 +49,13 @@ public class Assento {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
 	}
 }
