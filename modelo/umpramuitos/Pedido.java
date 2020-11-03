1c3d754257d11a777fec0d94e3725dc52fc6712c
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-11-03 20:43:52 -0300

    Update Pedido.java

--- modelo/umpramuitos/Pedido.java
+++ modelo/umpramuitos/Pedido.java
@@ -4,11 +4,32 @@ import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+/*
+ * A propriedade fetch pode ser usada sempre que há relacionamentos entre as
+ * entidades. Ela denota a prioridade com que os dados de entidades
+ * relacionadas são trazidos para a aplicação quando há pesquisa de um atributo
+ * com anotação de relacionamento. Usando essa entidade como exemplo: pedido
+ * possui uma lista de itens, sendo uma relação de um para muitos, por padrão,
+ * quando um pedido é acessado o java não traz os dados dos itens a ele
+ * relacionados. O JPA traz automaticamente apenas os dados de relacionamentos
+ * que terminam com um (um para um e muitos para um, usando relacão
+ * bidirecional), os itens serão somente recuperados do banco caso haja
+ * interação com eles. Usando fetch como EAGER fará com que todos os itens
+ * do pedido sejam trazidos do banco juntamente com os dados do pedido. Para
+ * alterar o comportamento padrão dos relacionamentos terminados em um, basta
+ * utilizar fetch = FetchType.LAZY para que os dados não sejam carregados
+ * automaticamente. Quando o EAGER está ativado, os dados dos relacionamentos
+ * são trazidos NA MESMA PESQUISA que os da entidade principal (mais rápido),
+ * mas se forem muitos dados, pode haver demora já que esse instrumento pode
+ * ser usado recursivamente entre as dependências de cada entidade.
+ */
 
 @Entity
 @Table(name = "PEDIDOS")
@@ -19,7 +40,7 @@ public class Pedido {
 	
 	private Date data;
 	
-	@OneToMany(mappedBy = "pedido")
+	@OneToMany(mappedBy = "pedido", fetch = FetchType.EAGER)
 	private List<ItemPedido> itens;
 
 	public Pedido() {
