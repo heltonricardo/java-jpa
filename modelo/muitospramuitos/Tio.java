f70b4578cb51ff013fae28d08ba49bdf5a4bd1b4
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-11-05 12:26:24 -0300

    Update Tio.java

--- modelo/muitospramuitos/Tio.java
+++ modelo/muitospramuitos/Tio.java
@@ -8,8 +8,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "TIOS")
 public class Tio {
 
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
