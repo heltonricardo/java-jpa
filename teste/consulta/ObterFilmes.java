6544740fd6baeb8948b8e4109f52369f9522a898
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-11-06 14:00:58 -0300

    Create ObterFilmes.java

---
+++ teste/consulta/ObterFilmes.java
@@ -0,0 +1,28 @@
+package teste.consulta;
+
+import java.util.List;
+
+import infra.DAO;
+import modelo.muitospramuitos.Ator;
+import modelo.muitospramuitos.Filme;
+
+public class ObterFilmes {
+
+	public static void main(String[] args) {
+		
+		DAO<Filme> dao = new DAO<>(Filme.class);
+		
+		List<Filme> filmes = dao.consultar("filmesNotaMaiorQue", "nota", 3.0);
+		
+		System.out.println("Quantidade: " + filmes.size());
+		
+		for (Filme filme : filmes) {
+			System.out.println();
+			
+			System.out.println(filme.getNome() + " -> " + filme.getNota());
+			
+			for (Ator ator: filme.getAtores())
+				System.out.println("\t" + ator.getNome());
+		}
+	}
+}
