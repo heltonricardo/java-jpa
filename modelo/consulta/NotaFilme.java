fe11c1778b7d1aab2207e29a21fb49ca34f2b460
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-11-07 15:47:44 -0300

    Create NotaFilme.java

---
+++ modelo/consulta/NotaFilme.java
@@ -0,0 +1,18 @@
+package modelo.consulta;
+
+public class NotaFilme {
+
+	private Double media;
+
+	public NotaFilme(Double media) {
+		this.media = media;
+	}
+
+	public Double getMedia() {
+		return media;
+	}
+
+	public void setMedia(Double media) {
+		this.media = media;
+	}
+}
